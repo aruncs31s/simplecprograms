f9cf78a34b31fc54c80231d88d928dbd2e2aa9c1
Arun cs <87601622+aruncs31s@users.noreply.github.com>
2022-01-08 12:06:44 +0530

    Add files via upload

---
+++ smallesttof2numbusingfunctions.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//smallest of 2 using function
+int small(int a,int b);
+int main()
+
+{
+    int a,b,smallest,num;
+    printf("Enter the Two values: \n");
+    scanf("%d %d",&a,&b);
+    if (a==b)
+    {
+        printf("both the values are same\n");
+    }
+    else
+    {
+    
+        smallest =  small(a, b);
+        printf("\nsmallest is %d\n",smallest);
+    }
+    return 0;
+
+}
+int small(int a,int b)
+{
+    int num;
+    if(a<b)
+    {
+        printf("1st Number is small ");
+        num=a;
+    }
+
+    else
+    {
+        printf("2nd Number is small ");
+        num=b;
+    }
+    return num;
+    
+}
